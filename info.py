a4b5746243c5a1a410d35c436f0f2a1cd8d70d7f
lplancarte <lplancarte@outlook.com>
2023-08-05 15:28:20 -0600

    Wrote the Basic commands section

---
+++ info.py
@@ -0,0 +1,44 @@
+"""
+----------Introduction to Bash Scripting:------------------
+What is Bash?
+    A shell language
+    Bourne Again Shell
+    Easy commands
+Why learn Bash?
+    Most used shell
+    Stands the test of time
+    Comes with Linux and other OS
+Why not Bash?
+    Lacks features
+    No OOP
+    Difficult syntax compared to Python
+    Newer tools like Ansible
+Still useful
+    Lightweight and always available
+    Is not mutually exclusive
+    Basic Knowledge makes big difference
+
+Will use WSL (Windows Subsystem for Linux)
+(Write every command in terminal from ubuntu)
+
+Basic commands-----------------------------------------
+echo: displays text you pass as an argument
+    echo hello
+cat: displays content of a file
+        But first we need to create a file:
+        vim textfile.txt
+            press i to enter insert mode
+            type whatever you want
+            pres esc to go back to command mode
+            :w
+                to write the file and save changes
+                :q
+                    to exit file
+                    or just do :wq
+                    :q! to exit without saving changes
+    cat textfile.txt
+
+Writing your first bash script------------------------------
+
+
+"""
