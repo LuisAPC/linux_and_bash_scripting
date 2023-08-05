ca2106899caebc32aa91bc46f97398ea158ab582
lplancarte <lplancarte@outlook.com>
2023-08-05 16:13:01 -0600

    Wrote Writing your first bash script section

--- info.py
+++ info.py
@@ -26,6 +26,7 @@ echo: displays text you pass as an argument
     echo hello
 cat: displays content of a file
         But first we need to create a file:
+    vim: text editor
         vim textfile.txt
             press i to enter insert mode
             type whatever you want
@@ -37,8 +38,51 @@ cat: displays content of a file
                     or just do :wq
                     :q! to exit without saving changes
     cat textfile.txt
+ls: list files in cur dir
+    ls -l: long format (-l is called a flag)
+pwd: print working directory
+bash: run shell script
+    bash name_of_file
+echo $SHELL:
+    veryfy in which shell you are running (bash in this case)
+chmod u+x name_of_file:
+    give user executable permission for a specific file
 
 Writing your first bash script------------------------------
+vim shelltest.sh
+    press a to enter append mode (just to show another way rather than i)
+        (this will append your text rather than insert it)
+        insert mode: write character before the cursor
+        append mode: write character after the cursor
+    write:
+        echo Hello World!
+    esc
+    :wq
+bash shelltest.sh
+    run our bash script
+echo $SHELL
+    to veryfy that we are running in a bash shell, so teretically we shouldnt
+    have to specify it, but is a best practice to do so
+    But good news is that we can enter this info inside our shell script
+    (type it at top of script)
+    In order to do that you have to provide the full path to the shell
+    interpreter. So copy the output path of the command echo $SHELL
+    and paste it in shel script
+vim shelltest.sh
+    the first line should be:
+    #!copied_path (#!/bin/bash)
+    (this is called the shebang: #!
+    which is basically telling shell script which interpreter to use)
+    save and go back to terminal (:wq)
+./shelltest.sh
+    to now run the file in a better way
+    permission will be denied, so you have to grant permissions:
+        chmod u+x shelltest.sh
+            grant permission to execute file only to user
+./shelltest.sh
+
+Variables--------------------------------------------------------------
+vim shelltest.sh
 
 
 """
